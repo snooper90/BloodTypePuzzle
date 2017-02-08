54bc1464d138114258c164e06c9059b09c437e02
Christopher Porter <snooper90@gmail.com>
2017-02-07 23:44:08 -0600

    fixed bug when two of the same empty positions tried to go on the same row. Added the splitBloodtypes function

--- bloodPuzzle.js
+++ bloodPuzzle.js
@@ -1,64 +1,131 @@
-"use strict"
 
-// Hardcoded board for a 5 x 5
-const gameBoard = [
-  ['','','','',''],
-  ['','','','',''],
-  ['','','','',''],
-  ['','','','',''],
-  ['','','','','']
-];
-const emptyPositions = [ 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 ];
+(function(){
+  "use strict"
 
-function insertEmptyPositions(emptyPositions) {
-  // Save possible empty positions in a shuffled array
-  let unusedEmptyPositions = shuffle(emptyPositions);
-  // Assign two empty positions per row
-  for( let i = 0; i < gameBoard.length; i++ ){
-    for (let j = 0; j < gameBoard.length - 3; j ++){
-      let index = unusedEmptyPositions.shift();
-        gameBoard[i][index] = "x";
-    }
-  }
-}
+  // Hardcoded board for a 5 x 5
+  let gameBoard = [
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','','']
+  ];
+  const emptyPositions = [ 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 ];
 
 
-function assignAllelePositions() {
-  for( let i = 0; i < gameBoard.length; i++ ){
-    for (let j = 0; j < gameBoard[i].length; j ++){
-      // if space is not supposed to be empty assign it a, b or o
-      if(gameBoard[i][j] !== "x"){
-        let randomNumber = Math.floor(Math.random() * 2);
-        let randomAllele;
-        switch (randomNumber) {
-          case 0:
-            randomAllele = "a"
-            break;
-          case 1:
-            randomAllele = "b"
-            break;
-          default:
-            randomAllele = "o"
+
+  function insertEmptyPositions(positions) {
+    // Save possible empty positions in a shuffled array
+    let unusedEmptyPositions = shuffle(positions.slice());
+    // Assign two empty positions per row
+    for( let i = 0; i < gameBoard.length; i++ ){
+      for (let j = 0; j < 2; j ++){
+        // If space is already empty reshuffle the positions
+        while (gameBoard[i][unusedEmptyPositions[0]] === "x"){
+          if (i === 4){
+            console.log("Impossible board configuration. \nResetting board and trying again");
+            gameBoard = gameBoard = [
+              ['','','','',''],
+              ['','','','',''],
+              ['','','','',''],
+              ['','','','',''],
+              ['','','','','']
+            ];
+            insertEmptyPositions(emptyPositions)
+
+            return
+          }
+          unusedEmptyPositions = shuffle(unusedEmptyPositions);
         }
-        gameBoard[i][j] = randomAllele;
+        let index = unusedEmptyPositions.shift();
+        // Assign next empty position to the column of current row
+        gameBoard[i][index] = "x";
       }
     }
   }
-}
 
-//  Fisher-Yates Shuffle
-function shuffle(array) {
-    let counter = array.length;
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
-        // Decrease counter by 1
-        counter--;
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+
+  function assignAllelePositions() {
+    for( let i = 0; i < gameBoard.length; i++ ){
+      for (let j = 0; j < gameBoard[i].length; j ++){
+        // if space is not supposed to be empty assign it a, b or o
+        if(gameBoard[i][j] !== "x"){
+          let randomNumber = Math.floor(Math.random() * 3);
+          let randomAllele;
+          switch (randomNumber) {
+            case 0:
+              randomAllele = "a"
+              break;
+            case 1:
+              randomAllele = "b"
+              break;
+            default:
+              randomAllele = "o"
+          }
+          gameBoard[i][j] = randomAllele;
+        }
+      }
     }
-    return array;
-}
+  }
+
+  //  Fisher-Yates Shuffle
+  function shuffle(array) {
+      let counter = array.length;
+      // While there are elements in the array
+      while (counter > 0) {
+          // Pick a random index
+          let index = Math.floor(Math.random() * counter);
+          // Decrease counter by 1
+          counter--;
+          // And swap the last element with it
+          let temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+      }
+      return array;
+  }
+
+  function findBloodTypes(){
+    let bloodTypeCol = [];
+    let bloodTypeRow = [];
+    // Get the arrays
+    for(let i = 0; i < gameBoard.length; i++){
+      for(let j = 0; j < gameBoard.length; j++){
+
+        if(gameBoard[i][j] !== "x"){
+          // will add left to right
+          bloodTypeRow.push(gameBoard[i][j])
+        };
+        if(gameBoard[j][i] !== "x"){
+          // will add top to bottom
+          bloodTypeCol.push(gameBoard[j][i])
+        };
+      };
+    };
+    let bloodTypesRow = splitBloodtypes(bloodTypeRow);
+    let bloodTypesCol = splitBloodtypes(bloodTypeCol);
+
+
+  }
+
+  function splitBloodtypes(arr){
+    let bloodTypes = [[],[],[],[],[]];
+    for( let i = 0; i < gameBoard.length; i++ ){
+      let subArr = arr.splice(0, 3);
+      // consider setting the bloodtype here
+      bloodTypes[i] = [[subArr[1], subArr[2]], [subArr[2], subArr[3]]]
+    }
+    return bloodTypes
+
+  }
+
+
+
+  insertEmptyPositions(emptyPositions);
+  assignAllelePositions();
+  findBloodTypes()
+
+
+
+
+})()
