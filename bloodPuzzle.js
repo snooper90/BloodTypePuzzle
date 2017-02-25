d1954f0c987e91be6bad087d16e69b5042af551f
Christopher Porter <snooper90@gmail.com>
2017-02-25 17:27:49 -0600

    Refactored to use ES6 Classes

--- bloodPuzzle.js
+++ bloodPuzzle.js
@@ -1,55 +1,66 @@
+"use strict"
+class BloodTypePuzzle {
+  constructor(){
+    //Currently hardcoded to be 5x5
+    this.emptyPositions =  [ 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 ];
+    this.gameBoard = [
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','',''],
+      ['','','','','']
+    ];
+    this.bloodTypesRow = [];
+    this.bloodTypesCol = [];
+    this.createGameSession()
+  }
 
-(function(){
-  "use strict"
+  createGameSession(){
+    // Fill with the steps to create the finished board
+    this.insertEmptyPositions(this.emptyPositions)
+    console.log(this.gameBoard);
+    this.assignAllelePositions()
+    console.log(this.gameBoard);
+    this.findBloodTypes()
+    console.log(this.bloodTypesCol);
+    console.log(this.bloodTypesRow);
 
-  // Hardcoded board for a 5 x 5
-  let gameBoard = [
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','','']
-  ];
-  const emptyPositions = [ 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 ];
+  }
 
-
-
-  function insertEmptyPositions(positions) {
+  insertEmptyPositions(positions) {
     // Save possible empty positions in a shuffled array
-    let unusedEmptyPositions = shuffle(positions.slice());
+    let unusedEmptyPositions = this.shuffle(positions.slice());
     // Assign two empty positions per row
-    for( let i = 0; i < gameBoard.length; i++ ){
+    for( let i = 0; i < this.gameBoard.length; i++ ){
       for (let j = 0; j < 2; j ++){
         // If space is already empty reshuffle the positions
-        while (gameBoard[i][unusedEmptyPositions[0]] === "x"){
+        while (this.gameBoard[i][unusedEmptyPositions[0]] === "x"){
           if (i === 4){
             console.log("Impossible board configuration. \nResetting board and trying again");
-            gameBoard = gameBoard = [
+            this.gameBoard = [
               ['','','','',''],
               ['','','','',''],
               ['','','','',''],
               ['','','','',''],
               ['','','','','']
             ];
-            insertEmptyPositions(emptyPositions)
-
+            this.insertEmptyPositions(emptyPositions)
             return
           }
-          unusedEmptyPositions = shuffle(unusedEmptyPositions);
+          unusedEmptyPositions = this.shuffle(unusedEmptyPositions);
         }
         let index = unusedEmptyPositions.shift();
         // Assign next empty position to the column of current row
-        gameBoard[i][index] = "x";
+        this.gameBoard[i][index] = "x";
       }
     }
   }
 
-
-  function assignAllelePositions() {
-    for( let i = 0; i < gameBoard.length; i++ ){
-      for (let j = 0; j < gameBoard[i].length; j ++){
+  assignAllelePositions() {
+    for( let i = 0; i < this.gameBoard.length; i++ ){
+      for (let j = 0; j < this.gameBoard[i].length; j ++){
         // if space is not supposed to be empty assign it a, b or o
-        if(gameBoard[i][j] !== "x"){
+        if(this.gameBoard[i][j] !== "x"){
           let randomNumber = Math.floor(Math.random() * 3);
           let randomAllele;
           switch (randomNumber) {
@@ -62,14 +73,14 @@
             default:
               randomAllele = "o"
           }
-          gameBoard[i][j] = randomAllele;
+          this.gameBoard[i][j] = randomAllele;
         }
       }
     }
   }
 
   //  Fisher-Yates Shuffle
-  function shuffle(array) {
+  shuffle(array) {
       let counter = array.length;
       // While there are elements in the array
       while (counter > 0) {
@@ -85,44 +96,38 @@
       return array;
   }
 
-  function findBloodTypes(){
+  findBloodTypes(){
     let bloodTypeCol = [];
     let bloodTypeRow = [];
     // Get the arrays
-    for(let i = 0; i < gameBoard.length; i++){
-      for(let j = 0; j < gameBoard.length; j++){
-
-        if(gameBoard[i][j] !== "x"){
+    for(let i = 0; i < this.gameBoard.length; i++){
+      for(let j = 0; j < this.gameBoard.length; j++){
+        if(this.gameBoard[i][j] !== "x"){
           // will add left to right
-          bloodTypeRow.push(gameBoard[i][j])
+          bloodTypeRow.push(this.gameBoard[i][j])
         };
-        if(gameBoard[j][i] !== "x"){
+        if(this.gameBoard[j][i] !== "x"){
           // will add top to bottom
-          bloodTypeCol.push(gameBoard[j][i])
+          bloodTypeCol.push(this.gameBoard[j][i])
         };
       };
     };
-    let bloodTypesRow = splitBloodtypes(bloodTypeRow);
-    let bloodTypesCol = splitBloodtypes(bloodTypeCol);
-    console.log(bloodTypeCol);
-
+    this.bloodTypesRow = this.splitBloodtypes(bloodTypeRow);
+    this.bloodTypesCol = this.splitBloodtypes(bloodTypeCol);
   }
 
-  function splitBloodtypes(arr){
+  splitBloodtypes(arr){
     let bloodTypes = [[],[],[],[],[]];
-    for( let i = 0; i < gameBoard.length; i++ ){
+    for( let i = 0; i < this.gameBoard.length; i++ ){
       let subArr = arr.splice(0, 3);
-      let firstBloodType = findBloodtype(subArr[0], subArr[1])
-      console.log("firstBloodType: " + firstBloodType);
-      let secondBloodType = findBloodtype(subArr[1], subArr[2])
+      let firstBloodType = this.findBloodtype(subArr[0], subArr[1])
+      let secondBloodType = this.findBloodtype(subArr[1], subArr[2])
       bloodTypes[i] = [firstBloodType, secondBloodType]
     };
-    console.log(bloodTypes);
     return bloodTypes
   }
 
-  function findBloodtype(a, b){
-    console.log("findBloodtype a & b : " + a + " " + b);
+  findBloodtype(a, b){
     if((a === "a" && b === "a") || (a === "o" && b === "a") || (a === "a" && b === "o")){
       return "A"
     }else if((a === "a" && b === "b") || (a === "b" && b === "a")){
@@ -134,11 +139,7 @@
     }
   }
 
-  insertEmptyPositions(emptyPositions);
-  assignAllelePositions();
-  findBloodTypes()
 
+}
 
-
-
-})()
+const game = new BloodTypePuzzle();
